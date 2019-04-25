9bf43b68e30c2a57598d6b862ef009d059132926
Gilbert Lucas <gilbertlucas46@gmail.com>
2019-04-25 17:16:40 +0800

    converted class based component to function with useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import Togggle from './Toggle'
 const App = () => {
+  const [count, Counter] = useState(0)
+  const [name, setName] = useState('')
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <button onClick={()=> Counter(count + 1) }>
+        {count} Clicked
+      </button>
+      <h3>{name}</h3>
+      <Togggle/>
+      <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
     </div>
   );
 };
